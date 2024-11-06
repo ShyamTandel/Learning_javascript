bb88670b4f776de9687f518ccf6d4bd3d7c882a2
Shyam Tandel <shyamtandel323@gmail.com>
2024-11-06 05:15:42 +0000

    01 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 55666
+let accountEmail = "shyamtandel@gmail.com"
+var accountPassword = "1234"
+accountCity = "Navsari"
+let accountState
+// accountId = 3
+accountEmail = "sdsfv@gmail.com"
+accountPassword = "65265"
+accountCity = "ahmedabad"
+
+
+/*prefer not to use var because of issue in block scope and functional scope */
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
